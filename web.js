599f57e8703c90ac061db400eb25c61d6d650e31
Ubuntu <balajis@stanford.edu>
2013-07-08 21:27:52 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
+var infile = "index.html";
+var out = "Takes stuff from index.html";
+var filebuf= fs.readFileSync(infile);
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World22!');
+  response.send(filebuf.toString());
 });
 
 var port = process.env.PORT || 5000;

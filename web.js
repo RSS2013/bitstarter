3d474bbcc1d1891a7086f6d343d89bb260683cc7
RSS2013 <RickySharma05@gmail.com>
2013-07-25 19:56:34 +0000

    added path to public static files

--- web.js
+++ web.js
@@ -3,13 +3,16 @@ var infile = "index.html";
 var out = "Takes stuff from index.html";
 var filebuf= fs.readFileSync(infile);
 var express = require('express');
+var path = require('path');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(filebuf.toString());
 });
 
-var port = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, 'public')));
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });

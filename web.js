587cd99f133dba50044f4b1879b43a3c9717493d
RSS2013 <RickySharma05@gmail.com>
2013-07-27 18:40:59 +0000

    added backend fetch of form data for emails, more fields to come

--- web.js
+++ web.js
@@ -7,12 +7,16 @@ var path = require('path');
 
 var app = express.createServer(express.logger());
 
+app.use(express.bodyParser());
+
+
 app.get('/', function(request, response) {
   response.send(filebuf.toString());
 });
 
-app.get('/success.html', function(request, response) {
-	response.send("You have submitted a registration request. Thank you!");
+app.post('/success', function(request, response) {
+	console.log(request.body.email);
+	response.end();
 });
 
 app.use(express.static(path.join(__dirname, 'public')));

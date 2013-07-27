2690d51aa2c516bb9575eb890ecfb39bb25950bf
wangshilianghit <wangshilianghit@gmail.com>
2013-07-27 12:20:12 -0400

    update

---
+++ public/assets/js/homepage.js
@@ -0,0 +1,23 @@
+  <script type="text/javascript">
+    $(document).ready(function(){
+        $("#signup").validate({
+            rules:{
+            email:{
+                required:true,
+                email: true
+            },
+            passwd:{
+                required:true,
+                minlength: 8
+            },
+            conpasswd:{
+                required:true,
+                equalTo: "#passwd"
+            },
+            },
+            errorClass: "help-inline"
+        });
+    });
+  </script>
+
+ 
